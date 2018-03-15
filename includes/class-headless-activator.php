12608d1aeafbd2f86251fe96e9fbf7ccf4620091
Matt Glaser <matt@alephsf.com>
2018-03-15 10:21:48 -0700

    first commit

---
+++ includes/class-headless-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://alephsf.com
+ * @since      1.0.0
+ *
+ * @package    Headless
+ * @subpackage Headless/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Headless
+ * @subpackage Headless/includes
+ * @author     Matt Glaser <matt@alephsf.com>
+ */
+class Headless_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-headless-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://alephsf.com
+ * @since      1.0.0
+ *
+ * @package    Headless
+ * @subpackage Headless/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Headless
+ * @subpackage Headless/includes
+ * @author     Matt Glaser <matt@alephsf.com>
+ */
+class Headless_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
